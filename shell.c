513ece0d8c66e0bce87967556b913e742673790b
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2023-05-24 19:02:50 -0700

    betty checks

--- shell.c
+++ shell.c
@@ -1,9 +1,16 @@
 #include "shell.h"
+/**
+ * main - This is the hub of the program
+ * @ac: argc
+ * @av: argv
+ * @env: environ variable
+ * Return: 0 on success
+ */
 
 int main(int ac, char **av, char **env)
 {
-        if (ac == 1)
-                launch_prompt(av, env);
+	if (ac == 1)
+		launch_prompt(av, env);
 
-        return (0);
+	return (0);
 }
