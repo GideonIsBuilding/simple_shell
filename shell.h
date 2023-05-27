431874fa79256e5a3da3af6b9d70ed39958529ce
Gideon Aleonogwe <galosikhena@gmail.com>
2023-05-27 01:21:54 +0100

    created the getline file

--- shell.h
+++ shell.h
@@ -7,6 +7,11 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stddef.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
 
 void launch_prompt(char **av, char **envv) __attribute__((unused));
 char *reading_input(void);
@@ -15,6 +20,7 @@ int cmp_chars(char str[], const char *delim);
 int str_len(const char *s);
 void free_char_array(char **args);
 char *str_tok(char *str, const char *delim);
+ssize_t my_getline(char **lineptr, size_t *n, int fd)
 
 
 
